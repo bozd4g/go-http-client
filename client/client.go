8af28e07d08a46707eea1ca279b6b2bb8a615a75
Furkan Bozdag <furkan@bozdag.dev>
2019-12-24 04:09:27 +0300

    Make some improvement

--- client/client.go
+++ client/client.go
@@ -20,58 +20,58 @@ type ServiceResponse struct {
 }
 
 func (h HttpClient) Get(endpoint string) ServiceResponse {
-	js, _ := json.Marshal(map[string] string{})
-	request, requestErr := http.NewRequest(http.MethodPost, fmt.Sprintf("%s%s", h.BaseUrl, endpoint), bytes.NewBuffer(js))
+	json, _ := json.Marshal(map[string] string{})
+	request, requestErr := http.NewRequest(http.MethodPost, fmt.Sprintf("%s%s", h.BaseUrl, endpoint), bytes.NewBuffer(json))
 
 	return parseResponse(request, requestErr)
 }
 
 func (h HttpClient) GetWithParameters(endpoint string, params interface{}) ServiceResponse {
-	js, _ := json.Marshal(map[string] string{})
+	json, _ := json.Marshal(map[string] string{})
 	queryString, _ := query.Values(params)
-	request, requestErr := http.NewRequest(http.MethodPost, fmt.Sprintf("%s%s?%s", h.BaseUrl, endpoint, queryString), bytes.NewBuffer(js))
+	request, requestErr := http.NewRequest(http.MethodPost, fmt.Sprintf("%s%s?%s", h.BaseUrl, endpoint, queryString), bytes.NewBuffer(json))
 
 	return parseResponse(request, requestErr)
 }
 
 func (h HttpClient) Post(endpoint string) ServiceResponse {
-	js, _ := json.Marshal(map[string] string{})
-	request, requestErr  := http.NewRequest(http.MethodPost, h.BaseUrl + endpoint, bytes.NewBuffer(js))
+	json, _ := json.Marshal(map[string] string{})
+	request, requestErr  := http.NewRequest(http.MethodPost, h.BaseUrl + endpoint, bytes.NewBuffer(json))
 
 	return parseResponse(request, requestErr)
 }
 
 func (h HttpClient) PostWithParameters(endpoint string, params interface{}) ServiceResponse {
-	js, _ := json.Marshal(params)
-	request, requestErr  := http.NewRequest(http.MethodPost, h.BaseUrl + endpoint, bytes.NewBuffer(js))
+	json, _ := json.Marshal(params)
+	request, requestErr  := http.NewRequest(http.MethodPost, h.BaseUrl + endpoint, bytes.NewBuffer(json))
 
 	return parseResponse(request, requestErr)
 }
 
 func (h HttpClient) Put(endpoint string) ServiceResponse {
-	js, _ := json.Marshal(map[string] string{})
-	request, requestErr := http.NewRequest(http.MethodPut, h.BaseUrl + endpoint, bytes.NewBuffer(js))
+	json, _ := json.Marshal(map[string] string{})
+	request, requestErr := http.NewRequest(http.MethodPut, h.BaseUrl + endpoint, bytes.NewBuffer(json))
 
 	return parseResponse(request, requestErr)
 }
 
 func (h HttpClient) PutWithParameters(endpoint string, params interface{}) ServiceResponse {
-	js, _ := json.Marshal(params)
-	request, requestErr := http.NewRequest(http.MethodPut, h.BaseUrl + endpoint, bytes.NewBuffer(js))
+	json, _ := json.Marshal(params)
+	request, requestErr := http.NewRequest(http.MethodPut, h.BaseUrl + endpoint, bytes.NewBuffer(json))
 
 	return parseResponse(request, requestErr)
 }
 
 func (h HttpClient) Delete(endpoint string) ServiceResponse {
-	js, _ := json.Marshal(map[string] string{})
-	request, requestErr := http.NewRequest(http.MethodDelete, h.BaseUrl + endpoint, bytes.NewBuffer(js))
+	json, _ := json.Marshal(map[string] string{})
+	request, requestErr := http.NewRequest(http.MethodDelete, h.BaseUrl + endpoint, bytes.NewBuffer(json))
 
 	return parseResponse(request, requestErr)
 }
 
 func (h HttpClient) DeleteWithParameters(endpoint string, params interface{}) ServiceResponse {
-	js, _ := json.Marshal(params)
-	request, requestErr := http.NewRequest(http.MethodDelete, h.BaseUrl + endpoint, bytes.NewBuffer(js))
+	json, _ := json.Marshal(params)
+	request, requestErr := http.NewRequest(http.MethodDelete, h.BaseUrl + endpoint, bytes.NewBuffer(json))
 
 	return parseResponse(request, requestErr)
 }
