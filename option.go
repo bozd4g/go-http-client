b9aea122e489871e6f4da09dda6675f8da2dab4c
bozd4g <bozd4g@gmail.com>
2022-10-22 17:31:32 +0200

    Add option.go

---
+++ option.go
@@ -0,0 +1,52 @@
+package gohttpclient
+
+import "time"
+
+type (
+	Option       func(c *Client)
+	ClientOption Option
+)
+
+func WithDefaultHeaders() ClientOption {
+	return func(c *Client) {
+		c.headers["Content-Type"] = Header{Value: "application/json", IsDefault: true}
+		c.headers["Accept"] = Header{Value: "application/json", IsDefault: true}
+	}
+}
+
+func WithLogger() ClientOption {
+	return func(c *Client) {}
+}
+
+func WithTimeout(timeout time.Duration) ClientOption {
+	return func(c *Client) {
+		c.timeout = timeout
+		c.httpClient.Timeout = timeout
+	}
+}
+
+func WithHeader(key, value string) Option {
+	return func(c *Client) {
+		if c.headers == nil {
+			c.headers = make(map[string]Header)
+		}
+
+		c.headers[key] = Header{Value: value, IsDefault: false}
+	}
+}
+
+func WithQuery(key, value string) Option {
+	return func(c *Client) {
+		if c.query == nil {
+			c.query = make(map[string]string)
+		}
+
+		c.query[key] = value
+	}
+}
+
+func WithBody(body []byte) Option {
+	return func(c *Client) {
+		c.body = body
+	}
+}
