d370d0b8eebcf7a21ec47b71128b5f8b97141307
bozd4g <bozd4g@gmail.com>
2022-11-05 20:02:24 +0100

    Add example.go

--- example/example.go
+++ example/example.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-	ghc "github.com/bozd4g/go-http-client"
+	"context"
+	"log"
+
+	gohttpclient "github.com/bozd4g/go-http-client"
 )
 
+type Post struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+}
+
 func main() {
-	_ = ghc.New("https://jsonplaceholder.typicode.com")
+	ctx := context.Background()
+	client := gohttpclient.New("https://jsonplaceholder.typicode.com")
+
+	response, err := client.Get(ctx, "/posts/1")
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	var post Post
+	if err := response.Json(&post); err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	log.Printf(post.Title) // sunt aut facere repellat provident occaecati...
 }
